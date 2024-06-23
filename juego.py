73762245171555d8315728a8a4e8fe899516c638
Tobias <tobiperea12@gmail.com>
2024-06-23 17:28:09 -0300

    Primeros Avances

--- juego.py
+++ juego.py
@@ -1,12 +1,21 @@
 import pygame
 
 pygame.init() #Se inicializa pygame
-imagen = pygame.image.load("god of questions.jfif")
-print(type(imagen))
 pantalla = pygame.display.set_mode([500, 500])
-pantalla.blit(imagen,(50,50))
 pygame.display.set_caption("God of Questions")
+imagen = pygame.image.load('C:/Users/12345/Desktop/segund par/god-of-questions.png')
+imagen = pygame.transform.scale(imagen, (500,500))
+carta_pregunta = pygame.Surface(((350,150)))
 
+pantalla.blit(imagen,(0,0))
+carta_pregunta = {"superficie":pygame.Surface((250,100)),"rectangulo":pygame.Rect(0,0,0,0)}
+carta_pregunta['superficie'].fill((250,125,51))
+carta_pregunta['rectangulo'] = pantalla.blit(carta_pregunta['superficie'],(120,200))
+jugar= "Jugar"
+fuente = pygame.font.SysFont("Arial Narrow", 48)
+texto = fuente.render(jugar, False, (0,0,0))
+pantalla.blit(texto,(200,230))
+pygame.display.flip()
 corriendo = True
 
 while corriendo:
@@ -15,3 +24,6 @@ while corriendo:
         if event.type == pygame.QUIT:
             print("SALIENDO")
             corriendo = False
+
+   
+
