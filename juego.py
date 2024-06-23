c423fdb4e9cc3d5757b0a142d0e9b5e295d12450
Tobias <tobiperea12@gmail.com>
2024-06-23 20:02:21 -0300

    pruebas

--- juego.py
+++ juego.py
@@ -1,16 +1,18 @@
 import pygame
 
+
 pygame.init() #Se inicializa pygame
-pantalla = pygame.display.set_mode([500, 500])
+pantalla_tamaño = (500, 500)
+pantalla = pygame.display.set_mode(pantalla_tamaño)
 pygame.display.set_caption("God of Questions")
 imagen = pygame.image.load('C:/Users/12345/Desktop/segund par/god-of-questions.png')
 imagen = pygame.transform.scale(imagen, (500,500))
 carta_pregunta = pygame.Surface(((350,150)))
 
 pantalla.blit(imagen,(0,0))
-carta_pregunta = {"superficie":pygame.Surface((250,100)),"rectangulo":pygame.Rect(0,0,0,0)}
+carta_pregunta = {"superficie":pygame.Surface((150,100)),"rectangulo":pygame.Rect(0,0,0,0)}
 carta_pregunta['superficie'].fill((250,125,51))
-carta_pregunta['rectangulo'] = pantalla.blit(carta_pregunta['superficie'],(120,200))
+carta_pregunta['rectangulo'] = pantalla.blit(carta_pregunta['superficie'],(200,230))
 jugar= "Jugar"
 fuente = pygame.font.SysFont("Arial Narrow", 48)
 texto = fuente.render(jugar, False, (0,0,0))
