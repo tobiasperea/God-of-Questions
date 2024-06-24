5a169c21eb379062a22876de0bf232e93d6ee0bc
Tabare <tabarepuglia@gmail.com>
2024-06-24 02:21:16 -0300

    Se prueba nuevo estilo de menu y se comenta

--- juego.py
+++ juego.py
@@ -1,22 +1,30 @@
 import pygame
 
 pygame.init() #Se inicializa pygame
+#------------------fondo----------------------------
 pantalla = pygame.display.set_mode([500, 500])
 pygame.display.set_caption("God of Questions")
-imagen = pygame.image.load("imagenes/god-of-questions.png")
-imagen = pygame.transform.scale(imagen, (500,500))
+imagen = pygame.image.load("imagenes/god-of-questions.png")#Se carga la imagen de fondo
+imagen = pygame.transform.scale(imagen, (500,500))#Se escala la imagen de fondo
+#---------------Cartas preguntas-----------------------
 carta_pregunta = pygame.Surface(((350,150)))
-
-pantalla.blit(imagen,(0,0))
-carta_pregunta = {"superficie":pygame.Surface((250,100)),"rectangulo":pygame.Rect(0,0,0,0)}
+carta_pregunta = {"superficie":pygame.Surface((150,55)),"rectangulo":pygame.Rect(0,0,0,0)}#se crea el marco que servira de objetivo
 carta_pregunta['superficie'].fill((250,125,51))
-carta_pregunta['rectangulo'] = pantalla.blit(carta_pregunta['superficie'],(120,200))
-jugar= "Jugar"
-fuente = pygame.font.SysFont("Arial Narrow", 48)
-texto = fuente.render(jugar, False, (0,0,0))
-pantalla.blit(texto,(200,230))
+carta_pregunta['rectangulo'] = pantalla.blit(carta_pregunta['superficie'],(170,220))
+pantalla.blit(imagen,(170,220)) # se carga el marco objetivo
+marco_pregunta = pygame.image.load("imagenes/marco.png") #se carga el marco de la pregunta que se vera
+pantalla.blit(imagen,(0,0))# se carga el fondo
+pantalla.blit(marco_pregunta,(40,250))#se carga el marco objetivo
+pantalla.blit(marco_pregunta,(40,380))
+pantalla.blit(marco_pregunta,(300,250))
+pantalla.blit(marco_pregunta,(300,380))
+#jugar= "Jugar"
+#fuente = pygame.font.SysFont("Arial Narrow", 48)
+#texto = fuente.render(jugar, False, (0,0,0))
+#pantalla.blit(texto,(200,230))
 pygame.display.flip()
 corriendo = True
+
 
 while corriendo:
    # Se verifica si el usuario cerro la ventana
@@ -24,6 +32,7 @@ while corriendo:
         if event.type == pygame.QUIT:
             print("SALIENDO")
             corriendo = False
+
 
    
 
