3ea8f87fe98449ba1d42035005de56693207b24c
Tabare <tabarepuglia@gmail.com>
2024-06-24 22:27:06 -0300

    prueba de funcion jugar y se actualizo imagenes

--- juego.py
+++ juego.py
@@ -7,19 +7,22 @@ pygame.display.set_caption("God of Questions")
 imagen = pygame.image.load("imagenes/god-of-questions.png")#Se carga la imagen de fondo
 imagen = pygame.transform.scale(imagen, (500,500))#Se escala la imagen de fondo
 #---------------Marco-----------------------
-marco_pregunta = pygame.image.load("imagenes/marco.png") #se carga el marco de la pregunta que se vera
+marco_jugar = pygame.image.load("imagenes/jugar.png") #se carga el marco de la pregunta que se vera
+marco_opciones = pygame.image.load("imagenes/opciones.png")
+marco_rankign = pygame.image.load("imagenes/ranking.png")
+marco_salir = pygame.image.load("imagenes/salir.png")
 #---------------Rectangulos-----------------------
-marco_rect1 = marco_pregunta.get_rect(topleft=(40, 250))
-marco_rect2 = marco_pregunta.get_rect(topleft=(40, 380))
-marco_rect3 = marco_pregunta.get_rect(topleft=(300, 250))
-marco_rect4 = marco_pregunta.get_rect(topleft=(300, 380))
+marco_rect1 = marco_jugar.get_rect(topleft=(40, 250))
+marco_rect2 = marco_rankign.get_rect(topleft=(40, 380))
+marco_rect3 = marco_opciones.get_rect(topleft=(300, 250))
+marco_rect4 = marco_salir.get_rect(topleft=(300, 380))
 
 #---------------Fondo y preguntas-----------------------
 pantalla.blit(imagen,(0,0))# se carga el fondo
-pantalla.blit(marco_pregunta, marco_rect1.topleft) #--se cargan los rectangulos
-pantalla.blit(marco_pregunta, marco_rect2.topleft)
-pantalla.blit(marco_pregunta, marco_rect3.topleft)
-pantalla.blit(marco_pregunta, marco_rect4.topleft)
+pantalla.blit(marco_jugar, marco_rect1.topleft) #--se cargan los rectangulos
+pantalla.blit(marco_rankign, marco_rect2.topleft)
+pantalla.blit(marco_opciones, marco_rect3.topleft)
+pantalla.blit(marco_salir, marco_rect4.topleft)
 #jugar= "Jugar"
 #fuente = pygame.font.SysFont("Arial Narrow", 48)
 #texto = fuente.render(jugar, False, (0,0,0))
@@ -28,8 +31,10 @@ pygame.display.flip()
 corriendo = True
 #---------------Musica-----------------------
 MUSICA_MENU.play(-1)
-
+aux = 1
 while corriendo:
+   #print(segundos)
+   contador = 0
    # Se verifica si el usuario cerro la ventana
    for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
@@ -38,12 +43,26 @@ while corriendo:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
             if marco_rect1.collidepoint(mouse_pos):
-                print("Marco 1 presionado")
+                segundos = pygame.time.get_ticks()/1000
+                if aux == segundos:
+                    aux +=1
+                pantalla.fill((0,0,0))
+                pygame.display.flip()
+                print("JUGAR")
+                pygame.mixer.stop()
+                musica_smash = pygame.mixer.Sound("musicaysonidos\zeus-aparece.mp3")
+                musica_smash.play()
+                zeus = pygame.image.load("imagenes/zeus.png")
+                duelo = pygame.image.load("imagenes/zeus-te-reta.png") 
+                pantalla.blit(duelo,(0,0))
+                pygame.display.flip()
+
             elif marco_rect2.collidepoint(mouse_pos):
-                print("Marco 2 presionado")
+                print("RANKING")
             elif marco_rect3.collidepoint(mouse_pos):
-                print("Marco 3 presionado")
+                print("OPCIONES")
             elif marco_rect4.collidepoint(mouse_pos):
-                print("Marco 4 presionado")
+                print("SALIENDO")
+                corriendo = False
    
 
